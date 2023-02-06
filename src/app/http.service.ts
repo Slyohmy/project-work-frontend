5cfd3de2c4cc5af14deece279a96cb4f3a61974b
slyohmy <simon.westlin@hotmail.com>
2023-02-07 00:05:33 +0100

    added http service for cors

---
+++ src/app/http.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getData(url: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+    return this.http.get(url, httpOptions);
+  }
+}
